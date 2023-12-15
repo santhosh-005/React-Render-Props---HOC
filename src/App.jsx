244c03d7470d4e4ac286892e7dc7041e85c44710
santhosh-005 <santhoshdon000555@gmail.com>
2023-12-15 15:36:29 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import './App.css';
-import LikeImage from './components/LikeImage';
-import LikePost from './components/LikePost';
+import "./App.css";
+import LikeImage from "./components/LikeImage";
+import LikeImagePart2 from "./components/LikeImagePart2";
+import LikePost from "./components/LikePost";
+import LikePostPart2 from "./components/LikePostPart2";
+import RenderProps from "./components/RenderProps";
 
 function App() {
   return (
     <div>
       <h3>Some Blog</h3>
-      <div className='buttons'>
-        <LikePost/>
-        <LikeImage/>
+      <div className="buttons">
+        {/* <LikePost/>
+        <LikeImage/> */}
+
+        <RenderProps
+          render={(count, handleCount) => (
+            <LikeImagePart2 count={count} handleCount={handleCount} />
+          )}
+        />
+
+        <RenderProps
+          render={(count, handleCount) => (
+            <LikePostPart2 count={count} handleCount={handleCount} />
+          )}
+        />
       </div>
     </div>
   );
--- src/components/LikeImage.jsx
+++ src/components/LikeImage.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
+import HOC from './HOC'
 
-export default function LikeImage() {
+function LikeImage(props) {
 
-  const [likeImageCounter, setLikeImageCounter] = useState(0);
 
-  const handleLikeImageCount = ()=>{
-    setLikeImageCounter(likeImageCounter+1);
-  }
+  const {likeImageCounter,handleLikeImageCount}=props
+  // const [likeImageCounter, setLikeImageCounter] = useState(0);
+
+  // const handleLikeImageCount = ()=>{
+  //   setLikeImageCounter(likeImageCounter+1);
+  // }
 
   return (
     <div>
@@ -14,3 +17,5 @@ export default function LikeImage() {
     </div>
   )
 }
+
+export default HOC(LikeImage)
